52d8faec0528886a0f8187e5928d98e19678ae59
Luieitalian <enderbud125@hotmail.com>
2024-04-04 18:15:39 +0300

    add bmp_config struct add BITINFOHEADER support

--- bmp_config.c
+++ bmp_config.c
@@ -1,21 +1,29 @@
 #include "bmp_config.h"
 
-CONFIG_T createEmptyConfig() {
-  CONFIG_T new_config;
-  new_config.pixel_offset = 0;
-  new_config.data_offset = 0;
-  new_config.optional_padding = 0;
+BMP_CONFIG_T *createEmptyBMPConfig() {
+  BMP_CONFIG_T *new_config = (BMP_CONFIG_T *)malloc(sizeof(BMP_CONFIG_T));
+
+  new_config->pixel_offset = 0;
+  new_config->data_offset = 0;
+  new_config->optional_padding = 0;
+  new_config->width_offset = 0;
+  new_config->height_offset = 0;
+  new_config->bits_per_pixel_offset = 0;
+
   return new_config;
 }
 
-CONFIG_T createConfig(BMP_CONFIGS_T bmp_config) {
-  CONFIG_T new_config;
+BMP_CONFIG_T *createBMPConfig(BMP_DIB_TYPE_T bmp_dib_type) {
+  BMP_CONFIG_T *new_config = (BMP_CONFIG_T *)malloc(sizeof(BMP_CONFIG_T));
 
-  switch (bmp_config) {
+  switch (bmp_dib_type) {
   case 0: // BITMAPINFOHEADER
-    new_config.pixel_offset = 0x3;
-    new_config.data_offset = 0x36;
-    new_config.optional_padding = 0x2;
+    new_config->pixel_offset = 0x3;
+    new_config->data_offset = 0x36;
+    new_config->optional_padding = 0x2;
+    new_config->width_offset = 0x12;
+    new_config->height_offset = 0x16;
+    new_config->bits_per_pixel_offset = 0x1C;
     break;
   }
 
--- bmp_config.h
+++ bmp_config.h
@@ -1,15 +1,20 @@
-#ifndef BMP_CONFIG
-#define BMP_CONFIG
+#ifndef BMP_CONFIG_H
+#define BMP_CONFIG_H
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef enum BMP_CONFIGS { BITMAPINFOHEADER } BMP_CONFIGS_T;
+typedef enum BMP_DIB_TYPE { BITMAPINFOHEADER } BMP_DIB_TYPE_T;
 
-typedef struct CONFIG {
+typedef struct BMP_CONFIG {
+  unsigned int width_offset;
+  unsigned int height_offset;
   unsigned int pixel_offset;
   unsigned int data_offset;
   unsigned int optional_padding;
-} CONFIG_T;
+  unsigned int bits_per_pixel_offset;
+} BMP_CONFIG_T;
 
-CONFIG_T createEmptyConfig();
-CONFIG_T createConfig(BMP_CONFIGS_T);
+BMP_CONFIG_T *createEmptyBMPConfig();
+BMP_CONFIG_T *createBMPConfig(BMP_DIB_TYPE_T bmp_dib_type);
 
 #endif
