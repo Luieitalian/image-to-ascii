c7dff9544f1ba1500817f7858f8f0c7593253e4f
Luieitalian <enderbud125@hotmail.com>
2024-04-04 18:16:08 +0300

    some testing

--- main.c
+++ main.c
@@ -1,41 +1,4 @@
-#include "bmp_config.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-#define BMP_WIDTH_OFFSET 0x12
-#define BMP_HEIGHT_OFFSET 0x16
-#define BMP_DATA_OFFSET 0xA
-#define BMP_BITS_PER_PIXEL 0x1C
-
-typedef struct Pixel {
-  int r, g, b, a;
-} Pixel_t;
-
-typedef struct BitMap {
-  Pixel_t **map;
-} BitMap_t;
-
-BitMap_t *createBitMap(FILE *image, int width, int height) {
-  BitMap_t *bitmap = (BitMap_t *)malloc(sizeof(BitMap_t));
-
-  for (int i = 0; i < height; i++) {
-    bitmap->map[i] = (Pixel_t *)malloc(sizeof(Pixel_t) * width);
-  }
-
-  return 0;
-}
-
-Pixel_t *createPixel(FILE *image, long offset) {
-  Pixel_t *p = (Pixel_t *)malloc(sizeof(Pixel_t));
-
-  fseek(image, offset, SEEK_SET);
-  printf("offset: %02lxh ", offset);
-  p->b = fgetc(image);
-  p->g = fgetc(image);
-  p->r = fgetc(image);
-
-  return p;
-}
+#include "bitmap.h"
 
 void printPixel(Pixel_t *pixel) {
   fflush(stdout);
@@ -53,40 +16,27 @@ void printRow(FILE *image, int width, long data_offset, int pixel_offset) {
 
 void printImage(FILE *image, int width, int height, long data_offset,
                 int pixel_offset) {
-  int padding = width % 4;
 
   for (int i = 0; i < height; i++) {
     printRow(image, width, data_offset + (i * pixel_offset * width),
              pixel_offset);
-    data_offset += padding;
+    data_offset += 0;
     printf("\n");
   }
 }
 
-void printImagev2(FILE *image, int width, int height, long data_offset) {}
-
 int main() {
+  FILE *image = fopen("bmps/example3.bmp", "r");
+  BMP_CONFIG_T *bmp_config = createBMPConfig(0); // BITMAPINFOHEADER
+  BitMap_t *bitmap = createBitMap(image, bmp_config);
 
-  FILE *image = fopen("example3.bmp", "r");
-  int img_width, img_height, img_pixel_offset;
-  long img_data_offset;
+  printf("width: %d\n", bitmap->width);
+  printf("height: %d\n", bitmap->height);
 
-  fseek(image, BMP_WIDTH_OFFSET,
-        SEEK_SET); // Get width of the bitmap in terms of pixels
-  img_width = fgetc(image);
-  fseek(image, BMP_HEIGHT_OFFSET,
-        SEEK_SET); // Get height of the bitmap in terms of pixels
-  img_height = fgetc(image);
+  printf("bitmap[1][1]: (%d %d %d)\n", bitmap->map[1][1].r, bitmap->map[1][1].g,
+         bitmap->map[1][1].b);
 
-  fseek(image, BMP_DATA_OFFSET, SEEK_SET);
-  img_data_offset = fgetc(image);
+  free(bmp_config);
 
-  fseek(image, BMP_BITS_PER_PIXEL, SEEK_SET);
-  img_pixel_offset = fgetc(image) / 8;
-
-  // fseek(image, 0x46, SEEK_SET);
-  // printf("%d\n", fgetc(image));
-
-  printImage(image, img_width, img_height, img_data_offset, img_pixel_offset);
   return 0;
 }
