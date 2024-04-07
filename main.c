4634bb7b24e9704f3a807cf86eb80112907707ef
Luieitalian <enderbud125@hotmail.com>
2024-04-07 21:52:45 +0300

    add examples & refactor main

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
+#include "ascii_matrix.h"
 #include "bitmap.h"
+#include "brightness.h"
+#include <stdio.h>
+#include <string.h>
 
-int main() {
-  FILE *image = fopen("bmps/example2.bmp", "r");
-  BMP_CONFIG_T *bmp_config = createBMPConfig(0); // BITMAPINFOHEADER
-  BitMap_t *bitmap = createBitMap(image, bmp_config);
+int main(int argc, char **argv) {
 
-  printBitMap(bitmap);
-  free(bmp_config);
+  char filename[30] = "bmps/";
+  strcat(filename, argv[1]);
 
+  BitMap_3_t *bitmap_3;
+  FILE *image = fopen(filename, "rb");
+  BMP_CONFIG_T *bmp_config = createBMPConfig(image);
+
+  // fseek(image, 0xE, SEEK_SET);
+  // printf("%d", fgetc(image));
+
+  bitmap_3 = createBitMap_3(image, bmp_config);
+  Brightness_Matrix_t *br_matrix = getBrightness_Matrix(bitmap_3);
+  //printBitMap_3(bitmap_3);
+  ASCII_Matrix_t *ascii_matrix = getASCII_Matrix(br_matrix);
+  printASCII_Matrix(ascii_matrix);
   return 0;
 }
--- makefile
+++ makefile
@@ -1,14 +1,16 @@
 all: compile link run
 
 compile:
-	gcc -c main.c  -o lib/main.o
+	gcc -c main.c -o lib/main.o
 	gcc -c bmp_config.c -o lib/bmp_config.o
 	gcc -c bitmap.c -o lib/bitmap.o
+	gcc -c brightness.c -o lib/brightness.o
+	gcc -c ascii_matrix.c -o lib/ascii_matrix.o
 
 link:
-	gcc lib/main.o lib/bmp_config.o lib/bitmap.o -o bin/main.exe
+	gcc lib/bmp_config.o lib/bitmap.o lib/brightness.o lib/ascii_matrix.o lib/main.o -o bin/main.exe
 
 run:
-	./bin/main.exe
+	./bin/main.exe $(img).bmp
 
 .PHONY: all compile link run
