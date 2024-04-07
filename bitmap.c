512eeb4951c7d1f1702b8bd7447d0c44c8e7dde9
Luieitalian <enderbud125@hotmail.com>
2024-04-07 21:50:27 +0300

    add partial dib support

--- bitmap.c
+++ bitmap.c
@@ -1,49 +1,92 @@
 #include "bitmap.h"
 #include <stdio.h>
 
-BitMap_t *createBitMap(FILE *image, BMP_CONFIG_T *bmp_config) {
-  BitMap_t *bitmap = (BitMap_t *)malloc(sizeof(BitMap_t));
+BitMap_3_t *createBitMap_3(FILE *image, BMP_CONFIG_T *bmp_config) {
+  BitMap_3_t *bitmap = (BitMap_3_t *)malloc(sizeof(BitMap_3_t));
   bitmap->bmp_config = bmp_config;
 
-  fseek(image, bitmap->bmp_config->width_offset,
-        SEEK_SET); // Get width of the bitmap in terms of pixels
-  bitmap->width = fgetc(image);
-  fseek(image, bitmap->bmp_config->height_offset,
-        SEEK_SET); // Get height of the bitmap in terms of pixels
-  bitmap->height = fgetc(image);
+  bitmap->map =
+      (Pixel_3_t **)malloc(sizeof(Pixel_3_t *) * bitmap->bmp_config->height);
 
-  bitmap->map = (Pixel_t **)malloc(sizeof(Pixel_t *) * bitmap->height);
-
-  for (unsigned int i = 0; i < bitmap->height; i++) {
-    bitmap->map[i] = (Pixel_t *)malloc(sizeof(Pixel_t) * bitmap->width);
+  for (unsigned int i = 0; i < bitmap->bmp_config->height; i++) {
+    bitmap->map[i] =
+        (Pixel_3_t *)malloc(sizeof(Pixel_3_t) * bitmap->bmp_config->width);
   }
 
-  hydrateBitMap(bitmap, image);
+  hydrateBitMap_3(bitmap, image);
 
   return bitmap;
 }
 
-void hydrateBitMap(BitMap_t *bitmap, FILE *image) {
-  fseek(image, bitmap->bmp_config->data_offset, SEEK_SET);
-  for (unsigned int i = 0; i < bitmap->height; i++) {
-    for (unsigned int j = 0; j < bitmap->width; j++) {
-      Pixel_t new_pixel;
+void hydrateBitMap_3(BitMap_3_t *bitmap, FILE *image) {
+  fseek(image, bitmap->bmp_config->bits_per_pixel_offset, SEEK_SET);
+  unsigned int padding = ((fgetc(image) / 8) * bitmap->bmp_config->width) % 4;
 
-      new_pixel.b = fgetc(image);
-      new_pixel.g = fgetc(image);
-      new_pixel.r = fgetc(image);
+  fseek(image, bitmap->bmp_config->data_offset, SEEK_SET);
+  for (unsigned int i = 0; i < bitmap->bmp_config->height; i++) {
+    for (unsigned int j = 0; j < bitmap->bmp_config->width; j++) {
+      Pixel_3_t new_pixel;
+
+      new_pixel.b = (unsigned char)fgetc(image);
+      new_pixel.g = (unsigned char)fgetc(image);
+      new_pixel.r = (unsigned char)fgetc(image);
 
       bitmap->map[i][j] = new_pixel;
     }
-    fseek(image, bitmap->bmp_config->optional_padding, SEEK_CUR);
+    fseek(image, padding, SEEK_CUR);
   }
 }
 
-void printBitMap(BitMap_t *bitmap) {
-  for (long i = bitmap->height - 1; i >= 0; i--) {
-    for (long j = 0; j < bitmap->width; j++) {
-      printf("(%d, %d, %d) ", bitmap->map[i][j].r, bitmap->map[i][j].g, bitmap->map[i][j].b);
-    }
+void printBitMap_3(BitMap_3_t *bitmap) {
+  for (long i = bitmap->bmp_config->height - 1; i >= 0; i--) {
     printf("-- \n\n-- ");
+    for (long j = 0; j < bitmap->bmp_config->width; j++) {
+      printf("(%d, %d, %d) ", bitmap->map[i][j].r, bitmap->map[i][j].g,
+             bitmap->map[i][j].b);
+    }
+  }
+}
+
+BitMap_4_t *createBitMap_4(FILE *image, BMP_CONFIG_T *bmp_config) {
+  BitMap_4_t *bitmap = (BitMap_4_t *)malloc(sizeof(BitMap_4_t));
+  bitmap->bmp_config = bmp_config;
+
+  bitmap->map =
+      (Pixel_4_t **)malloc(sizeof(Pixel_4_t *) * bitmap->bmp_config->height);
+
+  for (unsigned int i = 0; i < bitmap->bmp_config->height; i++) {
+    bitmap->map[i] =
+        (Pixel_4_t *)malloc(sizeof(Pixel_4_t) * bitmap->bmp_config->width);
+  }
+
+  hydrateBitMap_4(bitmap, image);
+
+  return bitmap;
+}
+
+void hydrateBitMap_4(BitMap_4_t *bitmap, FILE *image) {
+  fseek(image, bitmap->bmp_config->data_offset, SEEK_SET);
+
+  for (unsigned int i = 0; i < bitmap->bmp_config->height; i++) {
+    for (unsigned int j = 0; j < bitmap->bmp_config->width; j++) {
+      Pixel_4_t new_pixel;
+
+      // printf("bas: %lx\n", ftell(image));
+      new_pixel.b = (unsigned char)fgetc(image);
+      new_pixel.g = (unsigned char)fgetc(image);
+      new_pixel.r = (unsigned char)fgetc(image);
+      new_pixel.a = (unsigned char)fgetc(image);
+
+      bitmap->map[i][j] = new_pixel;
+    }
+  }
+}
+void printBitMap_4(BitMap_4_t *bitmap) {
+  for (long i = bitmap->bmp_config->height - 1; i >= 0; i--) {
+    printf("-- \n\n-- ");
+    for (long j = 0; j < bitmap->bmp_config->width; j++) {
+      printf("(%d, %d, %d, %d) ", bitmap->map[i][j].r, bitmap->map[i][j].g,
+             bitmap->map[i][j].b, bitmap->map[i][j].a);
+    }
   }
 }
--- bitmap.h
+++ bitmap.h
@@ -2,18 +2,29 @@
 #define BITMAP_H
 #include "bmp_config.h"
 
-typedef struct Pixel {
+typedef struct Pixel_3 {
   unsigned char r, g, b;
-} Pixel_t;
+} Pixel_3_t;
 
-typedef struct BitMap {
-  unsigned int width, height;
+typedef struct Pixel_4 {
+  unsigned char r, g, b, a;
+} Pixel_4_t;
+
+typedef struct BitMap_3 {
   BMP_CONFIG_T *bmp_config;
-  Pixel_t **map;
-} BitMap_t;
+  Pixel_3_t **map;
+} BitMap_3_t;
 
-BitMap_t *createBitMap(FILE *image, BMP_CONFIG_T *bmp_config);
-void hydrateBitMap(BitMap_t *bitmap, FILE *image);
-void printBitMap(BitMap_t *bitmap);
+typedef struct BitMap_4 {
+  BMP_CONFIG_T *bmp_config;
+  Pixel_4_t **map;
+} BitMap_4_t;
+
+BitMap_3_t *createBitMap_3(FILE *image, BMP_CONFIG_T *bmp_config);
+BitMap_4_t *createBitMap_4(FILE *image, BMP_CONFIG_T *bmp_config);
+void hydrateBitMap_3(BitMap_3_t *bitmap, FILE *image);
+void hydrateBitMap_4(BitMap_4_t *bitmap, FILE *image);
+void printBitMap_3(BitMap_3_t *bitmap);
+void printBitMap_4(BitMap_4_t *bitmap);
 
 #endif
