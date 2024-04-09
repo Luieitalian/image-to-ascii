ef3a7f25a0b79ca5d445fd658c606954aea822eb
Luieitalian <enderbud125@hotmail.com>
2024-04-09 21:33:48 +0300

    organise main

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
-#include "ascii_matrix.h"
-#include "bitmap.h"
-#include "brightness.h"
+#include "include/utils.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -9,17 +7,7 @@ int main(int argc, char **argv) {
   char filename[30] = "bmps/";
   strcat(filename, argv[1]);
 
-  BitMap_3_t *bitmap_3;
-  FILE *image = fopen(filename, "rb");
-  BMP_CONFIG_T *bmp_config = createBMPConfig(image);
+  openBMP(filename);
 
-  // fseek(image, 0xE, SEEK_SET);
-  // printf("%d", fgetc(image));
-
-  bitmap_3 = createBitMap_3(image, bmp_config);
-  Brightness_Matrix_t *br_matrix = getBrightness_Matrix(bitmap_3);
-  //printBitMap_3(bitmap_3);
-  ASCII_Matrix_t *ascii_matrix = getASCII_Matrix(br_matrix);
-  printASCII_Matrix(ascii_matrix);
   return 0;
 }
