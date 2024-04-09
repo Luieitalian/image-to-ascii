bb9f48ec434f604271882b4803f08c09bd71ba9d
Luieitalian <enderbud125@hotmail.com>
2024-04-09 21:54:21 +0300

    free memory after allocating

--- src/utils.c
+++ src/utils.c
@@ -2,13 +2,16 @@
 
 void openBMP(char *filename) {
 
-  BitMap_t *bitmap;
   FILE *image = fopen(filename, "rb");
   BMP_CONFIG_T *bmp_config = createBMPConfig(image);
 
-  bitmap = createBitMap(image, bmp_config);
+  BitMap_t *bitmap = createBitMap(image, bmp_config);
   Brightness_Matrix_t *br_matrix = getBrightness_Matrix(bitmap);
-
   ASCII_Matrix_t *ascii_matrix = getASCII_Matrix(br_matrix);
+
   printASCII_Matrix(ascii_matrix);
+
+  freeASCII_Matrix(ascii_matrix);
+  freeBrightness_Matrix(br_matrix);
+  freeBitmap(bitmap);
 }
